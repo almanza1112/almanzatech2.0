7421125f56a147e83de74a3323220acb1b924540
almanza1112 <almanza1112@gmail.com>
2022-07-15 13:35:21 -0400

    responsive text

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
           <div className='overlay'></div>
           <video src={bgVideo} autoPlay loop muted/>
             <div className='main-content'>
-              <div className='text-6xl font-bold'>WE BUILD YOUR VISION</div>  
+              <div className='text-2xl sm:text-6xl font-bold'>WE BUILD YOUR VISION</div>  
               <div>
                 <Link to="whatWeDo" smooth={true} duration={500}>
                   <button className='flat-btn my-2'>LEARN MORE ABOUT US</button>
