bbb99fe80d3fccdaa6bd9e2dfc87af6ed6546196
almanza1112 <almanza1112@gmail.com>
2022-07-15 13:36:54 -0400

    WhatWeDo init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Main from './components/Main';
+import WhatWeDo from './components/WhatWeDo'
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Main/>
+      <WhatWeDo/>
     </div>
 
    
