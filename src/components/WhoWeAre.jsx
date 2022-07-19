b2ef2aa8c14119590f2d66b217bc6bddbfc8ba9e
almanza1112 <almanza1112@gmail.com>
2022-07-18 22:44:01 -0400

    usable

--- src/components/WhoWeAre.jsx
+++ src/components/WhoWeAre.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 const WhoWeAre = () => {
   return (
-    <div className='section-style'>
-        
+    <div name='whoWeAre' className='section-style'>
+      <div className='max-w-screen-2xl w-full h-full mx-auto p-8 flex justify-center'>
+        <div className='w-full grid md:grid-cols-2 gap-8 items-center'>
+          <div>
+
+          </div>
+
+          <div className='flex flex-col'>
+            <div className='text-left'>
+              <p className='section-title'>Who We Are</p>
+            </div>
+
+            <div className='my-16'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
