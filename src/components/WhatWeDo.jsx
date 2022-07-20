c3aa143ed118db37fd19fdfbc2eb02994aba7f46
almanza1112 <almanza1112@gmail.com>
2022-07-20 11:40:15 -0400

    removed animations + dependecies

--- src/components/WhatWeDo.jsx
+++ src/components/WhatWeDo.jsx
@@ -2,18 +2,9 @@ import React from 'react'
 import {CgWebsite} from 'react-icons/cg'
 import {FaMobileAlt} from 'react-icons/fa'
 import {BsFillPeopleFill, BsTools} from 'react-icons/bs'
-import { useInView } from 'react-intersection-observer'
 
 const WhatWeDo = () => {
-    const options = {triggerOnce: true}
-    const {ref: btnRef, inView: btnVis} = useInView(options);
-    const {ref: pRef, inView: pVis} = useInView(options);
-    const {ref: wRef, inView: wVis} = useInView(options);
-    const {ref: mRef, inView: mVis} = useInView(options);
-    const {ref: iRef, inView: iVis} = useInView(options);
-    const {ref: cRef, inView: cVis} = useInView(options);
 
-    
   return (
     <div name='whatWeDo' className='section-style'>
         <div className='max-w-screen-2xl w-full h-full mx-auto p-8 justify-center flex'>
@@ -22,7 +13,7 @@ const WhatWeDo = () => {
                     <div className='text-left'>
                         <p className='section-title'>What We Do</p>
                     </div>
-                    <div ref={pRef} className={pVis ? 'animate-in slide-in-from-left my-8 md:my-16' : 'my-8 md:my-16 '}>
+                    <div className='my-8 md:my-16'>
                         <p>
                             Every business nowadays needs to have an internet presence as well as a good technological infrastructure to
                             market, scale, and adapt as your business grows. This is where we at AlmanzaTech excel in. We create and design 
@@ -32,12 +23,12 @@ const WhatWeDo = () => {
                     </div>
                     
                     <div >
-                        <button ref={btnRef} className={btnVis ? 'animate-in slide-in-from-left flat-btn' : 'flat-btn'}>GET A QUOTE</button>
+                        <button className='flat-btn'>GET A QUOTE</button>
                     </div>
                 </div>
                 <div className='grid md:grid-cols-2 gap-4'>
                     {/* item - websites */}
-                    <div ref={wRef} className={wVis ? 'wwd-item animate-in slide-in-from-right duration-500':'wwd-item '}>
+                    <div className='wwd-item'>
                         <div className='flex justify-center items-center'>
                             <div className='border-2 border-primary rounded-full p-2 md:p-4 '>
                                 <CgWebsite size={25} color='#5ce1e6' />
@@ -50,7 +41,7 @@ const WhatWeDo = () => {
                     </div>
 
                     {/* item - mobile applications */}
-                    <div ref={mRef} className={mVis ? 'wwd-item animate-in slide-in-from-right duration-500':'wwd-item '}>
+                    <div className='wwd-item'>
                         <div className='flex justify-center items-center'>
                             <div className='border-2 border-primary rounded-full p-2 md:p-4 '>
                                 <FaMobileAlt size={25} color='#5ce1e6' />
@@ -63,7 +54,7 @@ const WhatWeDo = () => {
                     </div>
 
                     {/* item - IT support */}
-                    <div ref={iRef} className={iVis ? 'wwd-item animate-in slide-in-from-right duration-500':'wwd-item '}>
+                    <div className='wwd-item'>
                         <div className='flex justify-center items-center'>
                             <div className='border-2 border-primary rounded-full p-2 md:p-4  '>
                                 <BsTools size={25} color='#5ce1e6' />
@@ -76,7 +67,7 @@ const WhatWeDo = () => {
                     </div>
 
                     {/* item - consulting */}
-                    <div ref={cRef} className={cVis ? 'wwd-item animate-in slide-in-from-right duration-500':'wwd-item '}>
+                    <div className='wwd-item'>
                         <div className='flex justify-center items-center'>
                             <div className='border-2 border-primary rounded-full p-2 md:p-4 '>
                                 <BsFillPeopleFill size={25} color='#5ce1e6' />
