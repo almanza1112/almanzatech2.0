689785652f6e176d9eb1e6339803beea9c58851d
almanza1112 <almanza1112@gmail.com>
2023-01-06 15:18:13 -0500

    typo

--- src/components/WhoWeAre.jsx
+++ src/components/WhoWeAre.jsx
@@ -14,7 +14,7 @@ const WhoWeAre = () => {
             <p className='section-title md:self-start'>Who We Are</p>
 
             <p className='my-8 md:my-16'>
-              Based in Nothern New Jersey, since 2019 AlmanzaTech has strived in exceeding the needs of individuals 
+              Based in Northern New Jersey, since 2019 AlmanzaTech has strived in exceeding the needs of individuals 
               and businesses looking for the best and customizable applications. We are a dedicated team that 
               believes in every final product we create should be a work of art. We treat you like family here
               and always try to ensure that you are 100% satisfied.           
